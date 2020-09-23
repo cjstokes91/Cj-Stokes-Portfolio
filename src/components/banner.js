9bb6f3b5664d4b851189f6f6aaae16c5f480d5bc
carl stokes <carlstokes@carls-MBP.lan>
2020-09-22 17:01:48 -0700

    image debugging

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,31 +1,32 @@
 import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
+// import { useStaticQuery, graphql } from 'gatsby'
+// import Img from 'gatsby-image'
 
 const Banner = () => {
-    const data = useStaticQuery(graphql`
-        query {
-          placeholderImage: file(relativePath: { eq: "solo.png" }) {
-            childImageSharp {
-              fluid(maxWidth: 300) {
-                ...GatsbyImageSharpFluid
-              } 
-            }
-          }
-        }
-      `)
+    //     const data = useStaticQuery(graphql`
+    //     query {
+    //       placeholderImage: file(relativePath: { eq: "solo.png" }) {
+    //         childImageSharp {
+    //           fluid(maxWidth: 300) {
+    //             ...GatsbyImageSharpFluid
+    //           } 
+    //         }
+    //       }
+    //     }
+    //   `)
+    // console.log(data);
     return (
         <div className="banner">
             <div className='container'>
                 <div className="row">
                     <div className='main-text'>CJ STOKES</div>
-                    <div className='main-image'>
+                    {/* <div className='main-image'>
                         <Img fluid={data.placeholderImage.childImageSharp.fluid} />
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
     )
 }
 
-export default Banner
+export default Banner;
