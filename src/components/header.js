aa9381364345c4a37ddd3ebf2871569dfc2addfb
carl stokes <carlstokes@carls-MBP.lan>
2020-09-29 15:54:12 -0700

    add icon to images

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,26 @@
-import { Link } from "gatsby"
 import React from "react"
+import { Link } from "gatsby"
 
-const Header = () => (
-  <header>
-    <div className="container">
-      <div className="inner-header">
-        <div className="logo">
-          <Link to="/">In Development</Link>
-        </div>
-        <div className="navigation">
-          <nav>
-            <Link to="/About">About</Link>
-            <Link to="/Work">Work</Link>
-            <Link to="/Contact">Contact</Link>
-          </nav>
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className="inner-header">
+          <div className="logo">
+            <Link to="/">In Development</Link>
+          </div>
+          <div className="icon-one">
+          </div>
+          <div className="navigation">
+            <nav>
+              <Link to="/About">About</Link>
+              <Link to="/Work">Work</Link>
+              <Link to="/Contact">Contact</Link>
+            </nav>
+          </div>
         </div>
       </div>
-    </div>
-  </header >
-)
-export default Header
+    </header >
+  )
+}
+export default Header;
